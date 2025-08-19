f38c8ebe76838ede2e9f6d6c72a820a323c58902
Ariel Tanami <arieltanami122@gmail.com>
2025-08-19 19:33:37 +0300

    Refactor tile creation to use class names for styling instead of inline background images

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ for (let row = 0; row < 30; row++) {
   for (let col = 0; col < 100; col++) {
     const tile = document.createElement("div");
 
-    if (row == 11) { // grass
-      tile.style.backgroundImage = "url('assets/images/blocks/grass.webp')";
-      tile.style.backgroundSize = 'cover';
-    } else if (row >= 12 && row <= 15) { // Dirt
-      tile.style.backgroundImage = "url('assets/images/blocks/dirt.webp')";
-      tile.style.backgroundSize = 'cover';
-    } else if (row >= 16 && row <= 28) { // Stone
-      tile.style.backgroundImage = "url('assets/images/blocks/stone.webp')";
-      tile.style.backgroundSize = 'cover';
-    } else if (row >= 29) { // bedrock
-      tile.style.backgroundImage = "url('assets/images/blocks/bedrock.webp')";
-      tile.style.backgroundSize = 'cover';
+    if (row == 11) {
+      // grass
+      tile.classList.add("grass");
+    } else if (row >= 12 && row <= 15) {
+      // Dirt
+      tile.classList.add("dirt");
+    } else if (row >= 16 && row <= 28) {
+      // Stone
+      tile.classList.add("stone");
+    } else if (row >= 29) {
+      // bedrock
+      tile.classList.add("bedrock");
     }
 
     container.appendChild(tile);
